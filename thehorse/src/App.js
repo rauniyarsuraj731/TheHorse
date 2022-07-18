024f29657d9fc9c037347f3fc67c98c99cdc598d
Suraj <rauniyar.suraj731@gmail.com>
2022-07-18 23:36:24 +0530

    homepage initiate

--- thehorse/src/App.js
+++ thehorse/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar';
+import {HomePage} from './components/HomePage/HomePage.jsx'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <HomePage/>
     </div>
   );
 }
---
+++ thehorse/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,24 @@
+import "./HomePage.css"
+import React from 'react'
+
+export  const HomePage = () => {
+  return (
+    <div>
+      <img src="https://cdn.shopify.com/s/files/1/0233/5133/files/Behind_banner_vid_1536x.jpg?v=1654816890" alt="moving image" />
+      <div>
+        <img src="https://cdn.shopify.com/s/files/1/0233/5133/files/4_2000x.jpg?v=1653526955" alt="" />
+      <img src="https://cdn.shopify.com/s/files/1/0233/5133/files/giphy-2_2000x.gif?v=1653526899" alt="" />
+      </div>
+      <img src="https://cdn.shopify.com/s/files/1/0233/5133/files/3TheHorse_V1_FINAL_1536x.jpg?v=1653370239" alt="" />
+      <h5>Shop by Category</h5>
+      <div>
+        <img src="https://cdn.shopify.com/s/files/1/0233/5133/files/watches_5d0469ce-b4a9-4851-b21c-b6196e1845dc_632x.jpg?v=1653527699" alt="" />
+        <img src="https://cdn.shopify.com/s/files/1/0233/5133/files/black_backpack_632x.gif?v=1653874903" alt="" />
+        <img src="https://cdn.shopify.com/s/files/1/0233/5133/files/bags_632x.jpg?v=1653960404" alt="" />
+      </div>
+      <img src="https://cdn.shopify.com/s/files/1/0233/5133/files/monday_resin2_1536x.jpg?v=1653543476" alt="" />
+      <img src="https://cdn.shopify.com/s/files/1/0233/5133/files/dress_tan_1536x.jpg?v=1653543395" alt="" />
+    </div>
+  )
+}
+
